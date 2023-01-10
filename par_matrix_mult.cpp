39e5902828b39f18263953e8a4ec719985724348
henriqueconte <henriqueconte6@gmail.com>
2023-01-10 16:07:34 +0100

    Removed unecessary sends and receives

--- par_matrix_mult.cpp
+++ par_matrix_mult.cpp
@@ -23,12 +23,12 @@ int matrixA[N][N];
 int matrixB[N][N];
 int resultMatrix[N][N];
 
-int GenerateRandomNumber(){
+int GenerateRandomNumber() {
 	return std::rand() % 9 + 1;
 }
 
 template<int rows, int cols> 
-void generateMatrixes(int (&matrix)[rows][cols]){
+void generateMatrixes(int (&matrix)[rows][cols]) {
 	for(int i = 0; i < cols; i ++){
         for(int j = 0; j < rows; j ++){
           matrix[i][j] = GenerateRandomNumber();
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 	int rows_num;
 	int workers_num;
 	int remainder;
-	int whole_part;
+	int intervalLength;
 	int message_tag;
 	int i;
 	int j;
@@ -67,15 +67,15 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
 
     // TODO: Create case where we run in only one process
-	if(communicator_size < 2){
+	if (communicator_size < 2) {
 		MPI_Abort(MPI_COMM_WORLD, NOT_ENOUGH_PROCESSES_NUM_ERROR);
 	}
 
     workers_num = communicator_size - 1;
-    whole_part = N / workers_num;
+    intervalLength = N / workers_num;
     remainder = N % workers_num;
 
-	if(process_rank == MASTER_RANK){
+	if (process_rank == MASTER_RANK) {
 		printf("%sGenerating matrixes%s\n", CYN, RESET);
 		
 		printf("\n%sGenerating matrix %sA%s with size %s%dx%d",CYN, GRN, CYN, RESET, N, N);
@@ -91,20 +91,20 @@ int main(int argc, char *argv[]) {
 		offset = 0;
 
 		message_tag = MASTER_TAG;
-		for(process_id = 1; process_id <= workers_num; process_id ++ ){
-			rows_num = process_id <= remainder ? whole_part + 1 : whole_part;
-			MPI_Send(&offset, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD);
-			MPI_Send(&rows_num, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD);
-			MPI_Send(&matrixA[offset][0], rows_num * N, MPI_INT, process_id, message_tag, MPI_COMM_WORLD);
-			offset += rows_num;
+		for (process_id = 1; process_id <= workers_num; process_id++) {
+                rows_num = process_id <= remainder ? intervalLength + 1 : intervalLength;
+                MPI_Send(&rows_num, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD);
+                MPI_Send(&matrixA[offset], rows_num * N, MPI_INT, process_id, message_tag, MPI_COMM_WORLD);
+                offset += rows_num;
 		}
         MPI_Bcast(&matrixB, N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
 		message_tag = WORKER_TAG;
-		for(process_id = 1; process_id <= workers_num; process_id ++){
-			MPI_Recv(&offset, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD, &status);
-			MPI_Recv(&rows_num, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD, &status);
-			MPI_Recv(&resultMatrix[offset][0], rows_num * N, MPI_INT, process_id, message_tag, MPI_COMM_WORLD, &status);
+        offset = 0;
+		for (process_id = 1; process_id <= workers_num; process_id ++) {
+                MPI_Recv(&rows_num, 1, MPI_INT, process_id, message_tag, MPI_COMM_WORLD, &status);
+                MPI_Recv(&resultMatrix[offset], rows_num * N, MPI_INT, process_id, message_tag, MPI_COMM_WORLD, &status);
+            offset += rows_num;
 		}
 		printf("\n%sResult %sA*B%s", CYN, GRN, RESET);
 		PrintMatrix(resultMatrix);
@@ -114,24 +114,24 @@ int main(int argc, char *argv[]) {
 		printf("\n%dx%d - %f seconds\n", N, N, diff);
 	} 
 	
-	if(process_rank != MASTER_RANK){
+	if (process_rank != MASTER_RANK) {
 		message_tag = MASTER_TAG;
-		MPI_Recv(&offset, 1, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD, &status);
 		MPI_Recv(&rows_num, 1, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD, &status);
 		MPI_Recv(&matrixA, rows_num * N, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD, &status);
         MPI_Bcast(&matrixB, N * N, MPI_INT, 0, MPI_COMM_WORLD);
 
-		for(k = 0; k < N; k ++){
-			for(i = 0; i < rows_num; i ++){
-				resultMatrix[i][k] = 0;
-				for(j = 0; j < N; j ++){
+        // printf("rows num: %d \n", rows_num);
+		for (k = 0; k < N; k ++) {
+			for (i = 0; i < rows_num; i ++) {
+                printf("Process rank: %d, Pair: (%d, %d) \n", process_rank, i, k);
+				for (j = 0; j < N; j ++) {
 					resultMatrix[i][k] += matrixA[i][j] * matrixB[j][k];
 				}
 			}
 		}
 
 		message_tag = WORKER_TAG;
-		MPI_Send(&offset, 1, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD);
+		// MPI_Send(&offset, 1, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD);
 		MPI_Send(&rows_num, 1, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD);
 		MPI_Send(&resultMatrix, rows_num * N, MPI_INT, MASTER_RANK, message_tag, MPI_COMM_WORLD);
 	}
